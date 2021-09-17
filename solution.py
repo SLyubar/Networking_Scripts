0c1ce4f4483bbce363ec85a46b31bcf123c7d8af
SLyubar <90281724+SLyubar@users.noreply.github.com>
2021-09-17 12:00:31 -0400

    Assignment2
    This assignment creates a web server TCP connection and exchanges messages and files along it.

--- solution.py
+++ solution.py
@@ -1,48 +1,49 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
 
-### welcome_assignment_answers
-### Input - All eight questions given in the assignment.
-### Output - The right answer for the specific question.
+def webServer(port=13331):
+  
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  #Prepare a server socket
+  serverSocket.bind(("", port))
+  serverSocket.listen(5)
 
-def welcome_assignment_answers(question):
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
-        answer = "mTLS"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 5
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
-    else:
-        pass
-    return(answer)
+  while True:
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
 
+      try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        print(filename)
+        f = open(filename[1:])
+        outputdata = f.read()
+        f.close()
+        #Send one HTTP header line into socket.
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
 
-# if __name__ == "__main__":
-#     debug_question = "Are encoding and encryption the same? - Yes/No"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "Is it possible to decode a message without a key? - Yes/No"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
-#     print(welcome_assignment_answers(debug_question))
-#     debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
-#     print(welcome_assignment_answers(debug_question))
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+      except IOError:
+        # Send response message for file not found (404)
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n").encode()
+        # #Close client socket
+        connectionSocket.close()
+
+    except (ConnectionResetError, BrokenPipeError):
+      pass
+
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+  webServer(13331)
+
